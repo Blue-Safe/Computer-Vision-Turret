2227199cb5e4a5b777ad87946a5e6639e328c9c2
Isaiah Smith <isaiahsmith1776@gmail.com>
2025-12-05 20:27:20 -0500

    Got Tracking working, and wrote test script for servo calibration

--- Main.py
+++ Main.py
@@ -1,6 +1,6 @@
 import cv2
 import numpy as np
-import matplotlib.pyplot as plt
+
 
 
 def main():
@@ -23,7 +23,42 @@ def main():
 
         blur = cv2.GaussianBlur(grayScale,(15,15), 0)
 
-        cv2.imshow("Webcam",blur)
+        thresh = cv2.adaptiveThreshold(blur,255,cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY_INV,31,5)
+        
+        mask = thresh
+
+        contours, _ = cv2.findContours(mask,cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+        MIN_AREA = 800
+
+        # Meaningfull objects
+        MO = []
+
+        for object in contours:
+            if cv2.contourArea(object) > MIN_AREA:
+                MO.append(object)
+
+        visualize = frame.copy()
+        cv2.drawContours(visualize,MO,-1,(0,255,0),2)
+
+
+        targetCenter = None
+
+        if MO:
+            target = max(MO,key=cv2.contourArea)
+            
+            x,y,w,h = cv2.boundingRect(target)
+            
+            cx,cy = x + w//2, y+ h//2
+
+            targetCenter = (cx,cy)
+        
+        cv2.rectangle(frame, (x,y), (x+w, y+h), (0,0,255),2)
+        cv2.circle(frame, (cx,cy), 5, (0,255,0), -1)
+	
+
+
+        cv2.imshow('Webcam',frame)
 
         if cv2.waitKey(1) * 0xFF == ord('q'):
             break
---
+++ ServoTesting.py
@@ -0,0 +1,42 @@
+import pigpio, time, os
+
+pi = pigpio.pi()
+PIN_PAN = 18
+
+pi.set_mode(PIN_PAN, pigpio.OUTPUT)
+
+def setPWM(pin,time):
+    pi.set_servo_pulsewidth(pin,time)
+
+while True:
+    os.system("clear")
+    pin = input("Axis: ")
+    if pin.lower() == "x":
+        pin = 3
+    elif pin.lower() == "y":
+        pin = 5
+    else:
+        print("Invalid; 'x' or 'y' ")
+        time.sleep(2)
+        continue
+    print()
+    angle = int(input("Angle: "))
+    if angle == "0":
+        angle = 1000
+    elif angle == "90":
+        angle = 1500
+    elif angle == "180":
+        angle = 2000
+    else:
+        print("Invalid; '0','90','180'")
+        time.sleep(2)
+        continue
+
+    setPWM(pin,angle)
+    time.sleep(1)
+    print()
+    input("Hit enter to leave")
+
+
+
+
